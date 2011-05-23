b1a9c315bd5fe61f7b4129a983a7164d85e2cea4
Daniel Marschall <info@daniel-marschall.de>
2011-05-23 02:06:41 +0000

    Neue Sign-Batches
    git-svn-id: https://svn.viathinksoft.com/svn/delphiutils/trunk@65 a8afdbff-0e74-4854-8501-1ae6bb219a0b

--- _Batch/processDir.bat
+++ _Batch/processDir.bat
@@ -2,6 +2,11 @@
 
 echo === Process %~1 ===
 
-call "%%~dp0%%\cleanDir.bat" "%~1"
-call "%%~dp0%%\signExe.bat" "%~1\*.exe"
-call "%%~dp0%%\signExe.bat" "%~1\*.dll"
+call "%~dp0\cleanDir.bat" "%~1"
+call "%~dp0\signExe.bat" "%~1\*.exe"
+call "%~dp0\signExe.bat" "%~1\*.dll"
+call "%~dp0\signExe.bat" "%~1\*.ocx"
+call "%~dp0\signExe.bat" "%~1\*.msi"
+call "%~dp0\signExe.bat" "%~1\*.cab"
+call "%~dp0\signExe.bat" "%~1\*.xpi"
+call "%~dp0\signExe.bat" "%~1\*.xap"
--- _Batch/signExe.bat
+++ _Batch/signExe.bat
@@ -1,15 +1,4 @@
-@echo on
-
-SET XXX=%~f1
-
-cd "%~dp0"
-
-if not exist "%XXX%" goto end
-
-REM Bereits signiert?
-signtool.exe verify /pa "%XXX%"
-
-IF %ERRORLEVEL% == 0 GOTO end
+@echo off
 
 rem SET TSA=http://timestamp.verisign.com/scripts/timstamp.dll
 SET TSA=http://time.certum.pl/
@@ -17,8 +6,26 @@ SET TSA=http://time.certum.pl/
 SET NAME=ViaThinkSoft OpenSource Application
 SET URL=http://www.viathinksoft.de/
 
-signtool.exe sign -d "%NAME%" -du "%URL%" -a -t "%TSA%" "%XXX%"
+for %%i in (%1) do (
 
-:end
+echo.
+echo Found %%i
+echo Extended: %%~fi
+echo.
+
+cd "%~dp0"
+
+REM Bereits signiert?
+signtool.exe verify /pa "%%~fi"
+
+rem IF %ERRORLEVEL% == 0 GOTO end
+
+IF ERRORLEVEL 1 (
+
+signtool.exe sign -d "%NAME%" -du "%URL%" -a -t "%TSA%" "%%~fi"
+
+)
 
 cd ..
+
+)
