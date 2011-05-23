6c16d1a1a75e10794babefbd33134c671814c1e3
Daniel Marschall <info@daniel-marschall.de>
2011-05-23 21:57:51 +0000

    git-svn-id: https://svn.viathinksoft.com/svn/delphiutils/trunk@66 a8afdbff-0e74-4854-8501-1ae6bb219a0b

--- _Batch/processDir.bat
+++ _Batch/processDir.bat
@@ -2,11 +2,11 @@
 
 echo === Process %~1 ===
 
-call "%~dp0\cleanDir.bat" "%~1"
-call "%~dp0\signExe.bat" "%~1\*.exe"
-call "%~dp0\signExe.bat" "%~1\*.dll"
-call "%~dp0\signExe.bat" "%~1\*.ocx"
-call "%~dp0\signExe.bat" "%~1\*.msi"
-call "%~dp0\signExe.bat" "%~1\*.cab"
-call "%~dp0\signExe.bat" "%~1\*.xpi"
-call "%~dp0\signExe.bat" "%~1\*.xap"
+call "%~dp0cleanDir.bat" "%~1\"
+call "%~dp0signExe.bat" "%~1\*.exe"
+call "%~dp0signExe.bat" "%~1\*.dll"
+call "%~dp0signExe.bat" "%~1\*.ocx"
+call "%~dp0signExe.bat" "%~1\*.msi"
+call "%~dp0signExe.bat" "%~1\*.cab"
+call "%~dp0signExe.bat" "%~1\*.xpi"
+call "%~dp0signExe.bat" "%~1\*.xap"
--- _Batch/signExe.bat
+++ _Batch/signExe.bat
@@ -1,31 +1,27 @@
 @echo off
 
-rem SET TSA=http://timestamp.verisign.com/scripts/timstamp.dll
+REM SET TSA=http://timestamp.verisign.com/scripts/timstamp.dll
 SET TSA=http://time.certum.pl/
 
 SET NAME=ViaThinkSoft OpenSource Application
 SET URL=http://www.viathinksoft.de/
 
+echo Signing %1
+
 for %%i in (%1) do (
+	echo.
+	echo Found %%i
+	echo Extended: %%~fi
+	echo.
 
-echo.
-echo Found %%i
-echo Extended: %%~fi
-echo.
+	cd "%~dp0"
 
-cd "%~dp0"
+	REM Bereits signiert?
+	signtool.exe verify /pa "%%~fi"
 
-REM Bereits signiert?
-signtool.exe verify /pa "%%~fi"
+	IF ERRORLEVEL 1 (
+		signtool.exe sign -d "%NAME%" -du "%URL%" -a -t "%TSA%" "%%~fi"
+	)
 
-rem IF %ERRORLEVEL% == 0 GOTO end
-
-IF ERRORLEVEL 1 (
-
-signtool.exe sign -d "%NAME%" -du "%URL%" -a -t "%TSA%" "%%~fi"
-
-)
-
-cd ..
-
+	cd ..
 )
